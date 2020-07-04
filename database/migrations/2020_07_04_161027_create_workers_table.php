7ef1f9229ea5b5fd6f3db2101247297d3c489bf5
Sprrwkk <sprrwkk@gmail.com>
2020-07-04 20:33:10 +0200

    Create workers shema

---
+++ database/migrations/2020_07_04_161027_create_workers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id('worker_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday');
+            $table->bigInteger('position')->unsigned();
+            $table->foreign('position')
+                ->references('position_id')
+                ->on('positions')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}
