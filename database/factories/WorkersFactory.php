c2c025fb7debb309786aa65b7dbfa7a283026db3
Sprrwkk <sprrwkk@gmail.com>
2020-07-04 20:30:28 +0200

    WorkersFactory && adding fakers data functionality

---
+++ database/factories/WorkersFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Worker::class, function (Faker $faker) {
+
+    return [
+        'first_name' => $faker->name,
+        'last_name' => $faker->lastName,
+        'birthday' => $faker->dateTimeBetween('-50 years', '-20 years'),
+        'position' => $faker->numberBetween(1, App\Position::all()->count())
+        //
+    ];
+});
