be3ec792690197523defa85fc630e28bff83b67e
Sprrwkk <sprrwkk@gmail.com>
2020-07-06 21:12:19 +0200

    Add active $faker

--- database/factories/WorkersFactory.php
+++ database/factories/WorkersFactory.php
@@ -11,7 +11,8 @@ $factory->define(\App\Worker::class, function (Faker $faker) {
         'first_name' => $faker->name,
         'last_name' => $faker->lastName,
         'birthday' => $faker->dateTimeBetween('-50 years', '-20 years'),
-        'position' => $faker->numberBetween(1, App\Position::all()->count())
+        'position' => $faker->numberBetween(1, App\Position::all()->count()),
+        'active' => $faker->numberBetween(0, 1),
         //
     ];
 });
