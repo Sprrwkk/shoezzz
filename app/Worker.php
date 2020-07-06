fe2556fd01bfff04f39b6f173f7a3d48b777b8aa
Sprrwkk <sprrwkk@gmail.com>
2020-07-06 21:06:53 +0200

    Declare $primaryKey

--- app/Worker.php
+++ app/Worker.php
@@ -4,11 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class Worker extends Model
+
+
+
 {
     //
+    protected $primaryKey = 'worker_id';
+
     public function position()
     {
         return $this->belongsTo('App\Position', 'position', 'position_id');
     }
 }
+
+
