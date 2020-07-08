950836df35e41f197311d442e3e0afc4e4ee031d
Sprrwkk <sprrwkk@gmail.com>
2020-07-08 19:24:23 +0200

    Add getCategoryById() function && Add createCategory() function && add editCategory() function && add removeCategory() function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,8 +14,53 @@ class CategoryController extends Controller
         $categories = Category::all();
 
         return response($categories, 200);
-
-
     }
 
+
+
+    function getCategoryById($category_id) {
+
+        $category = Category::findOrFail($category_id);
+
+        return response($category, 200);
+    }
+
+
+
+    function createCategory(Request $request) {
+
+        $category = new Category;
+
+        $category->category_name = $request->category_name;
+        $category->save();
+
+        return response($category, 200);
+    }
+
+
+
+    function editCategory($category_id, Request $request) {
+
+        $category = Category::findOrFail($category_id);
+
+
+        $category->category_name = $request->category_name;
+
+        $category->save();
+
+        return response($category, 200);
+    }
+
+
+
+    function removeCategory($category_id) {
+
+        $category = Category::findOrFail($category_id);
+
+        $category->delete();
+
+        return response(200);
+    }
 }
+
+
