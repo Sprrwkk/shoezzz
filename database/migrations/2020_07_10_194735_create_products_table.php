5541778e0befff31412b9a0866bd20857dfad405
Sprrwkk <sprrwkk@gmail.com>
2020-07-10 22:04:28 +0200

    Add products table schema

---
+++ database/migrations/2020_07_10_194735_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_name');
+            $table->double('size')->nullable();
+            $table->double('price');
+            $table->BigInteger('category')->unsigned();
+            $table->foreign('category')
+                ->references('category_id')
+                ->on('categories')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
