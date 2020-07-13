28ffbf58a356bc5175fc57109a458235338b2c0b
Sprrwkk <sprrwkk@gmail.com>
2020-07-13 18:23:38 +0200

    Add ShowBrands() method  && Add showBrandById() method && Add createBrand() method && Add editBrand() method && Add removeBrandById() method

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Brand;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
     //
+    public function showBrands() {
+
+        $brands = Brand::all();
+
+        return response($brands, 200);
+    }
+
+
+    public function showBrandById($brand_id) {
+        $brand = Brand::findOrFail($brand_id);
+
+        return response($brand, 200);
+    }
+
+
+    public function createBrand(Request $request) {
+        $brand = new Brand;
+
+        $input = $request->all();
+
+        foreach ($input as $key=>$value) {
+            $brand -> $key = $input[$key];
+        }
+
+        $brand -> save();
+
+        return response($brand, 200);
+    }
+
+
+    public function editBrand($brand_id, Request $request) {
+        $brand = Brand::findOrFail($brand_id);
+
+        $input = $request->all();
+
+        foreach ($input as $key=>$value) {
+            $brand->$key = $input[$key];
+        }
+
+        $brand -> save();
+
+        return response($brand, 200);
+    }
+
+
+    public function removeBrandById($brand_id) {
+        $brand = Brand::findOrFail($brand_id);
+
+        $brand->delete();
+
+        return response(200);
+    }
+
 }
