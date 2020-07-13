72fb76b1cd92ac69e070df3822ea7cbfcefce427
Sprrwkk <sprrwkk@gmail.com>
2020-07-13 18:23:56 +0200

    Set primaryKey to brand_id

--- app/Brand.php
+++ app/Brand.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     //
+    protected $primaryKey = 'brand_id';
+
     public function product() {
         return $this->hasMany(Product::class);
     }
